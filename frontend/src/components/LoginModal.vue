1c13ea3f200f24e096aceae086103768d6c4d833
Tomas Trnecka <trnecka@gmail.com>
2025-08-03 17:13:44 +0200

    styling

--- frontend/src/components/LoginModal.vue
+++ frontend/src/components/LoginModal.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="modal show d-block" tabindex="-1" style="background: rgba(0,0,0,0.5)">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-sm">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Login</h5>
+          <h5 class="modal-title fs-6">Login</h5>
         </div>
         <div class="modal-body">
           <form @submit.prevent="submitLogin">
             <div class="mb-3">
-              <label>Login</label>
-              <input v-model="username" type="text" class="form-control" required />
+              <input v-model="username" type="text" class="form-control form-control-sm" placeholder="Login" required />
             </div>
             <div class="mb-3">
-              <label>Password</label>
-              <input v-model="password" type="password" class="form-control" required />
+              <input v-model="password" type="password" class="form-control form-control-sm" placeholder="Password" required />
             </div>
             <div class="modal-footer">
               <button type="submit" class="btn btn-primary">Login</button>
